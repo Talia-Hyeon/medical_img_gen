d16c14176c72293faf5ec9a963020f4a29c22c94
jihyeon <030310jhkim@naver.com>
2023-08-07 19:46:53 +0900

    modify threshold

--- deepInversion_3d_mask.py
+++ deepInversion_3d_mask.py
@@ -215,7 +215,7 @@ def gen_img_args():
 if __name__ == '__main__':
     # device
     os.environ['CUDA_DEVICE_ORDER'] = 'PCI_BUS_ID'
-    os.environ['CUDA_VISIBLE_DEVICES'] = '0,1,2,3,4,5'
+    os.environ['CUDA_VISIBLE_DEVICES'] = '0,1,2,3,4'
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
     # parser
