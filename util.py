c44f03ae5852e2fd106e5a75976bada8e7d41047
Talia-Hyeon <030310jhkim@naver.com>
2024-01-20 18:29:57 +0900

    add visualization, find_best_view, decode_segmap, get_train_transform, my_collate

--- util.py
+++ util.py
@@ -1,5 +1,14 @@
 import os
+
+import numpy as np
+import matplotlib.pyplot as plt
 import torch
+from batchgenerators.transforms.color_transforms import BrightnessMultiplicativeTransform, GammaTransform, \
+    BrightnessTransform, ContrastAugmentationTransform
+from batchgenerators.transforms.noise_transforms import GaussianNoiseTransform, GaussianBlurTransform
+from batchgenerators.transforms.resample_transforms import SimulateLowResolutionTransform
+from batchgenerators.transforms.abstract_transforms import Compose
+
 from model.unet3D import UNet3D
 
 
@@ -14,12 +23,133 @@ def save_model(path, model, optim, lr_sch, epoch):
     torch.save(states, path)
 
 
-def load_model(train_type, task_id):
-    pre_path = f'./save_model/{train_type}/{task_id - 1}/last_model.pth'
+def load_model(train_type, task_id, check_point=False):
+    pre_path = f'./save_model/{train_type}/{task_id}/last_model.pth'
     if os.path.exists(pre_path) != True:
         dirname, basename = os.path.split(pre_path)
         pre_path = dirname + '/best_model.pth'
-    pretrained = UNet3D(num_classes=task_id)
+
+    pretrained = UNet3D(num_classes=task_id + 1)
     checkpoint = torch.load(pre_path)
     pretrained.load_state_dict(checkpoint['model'], strict=False)
-    return pretrained
+
+    if check_point == True:
+        return pretrained, checkpoint
+    else:
+        return pretrained
+
+
+def visualization(img, label, root, name, num_classes):
+    # delete batch
+    image = torch.squeeze(img).numpy()
+    label = torch.squeeze(label).numpy()
+    label = np.argmax(label, axis=0)
+
+    # slice into the best view
+    max_score_idx = find_best_view(label, num_classes)
+    image = image[max_score_idx, :, :]
+    label = label[max_score_idx, :, :]
+    col_label = decode_segmap(label, num_classes)
+
+    plt.figure()
+    plt.subplot(1, 2, 1)
+    plt.imshow(image, cmap='gray')
+    plt.title('Image')
+    plt.subplot(1, 2, 2)
+    plt.imshow(col_label)
+    plt.title('Prediction')
+    plt.savefig(f'{root}/{name}.png')
+
+    plt.close()
+
+    # d, h, w = label.shape
+    # for i in range(d):
+    #     image_i = image[i, :, :]
+    #     label_i = label[i, :, :]
+    #     col_label = decode_segmap(label_i, num_classes)
+    #
+    #     plt.figure()
+    #     plt.subplot(1, 2, 1)
+    #     plt.imshow(image_i, cmap='gray')
+    #     plt.title('Image')
+    #     plt.subplot(1, 2, 2)
+    #     plt.imshow(col_label)
+    #     plt.title('Prediction')
+    #     plt.savefig(f'{root}/{name}_{i}.png')
+    #
+    #     plt.close()
+
+
+def find_best_view(img, num_classes):
+    d1, d2, d3 = img.shape
+    max_score = 0
+    max_score_idx = 0
+    for i in range(d1):
+        sagital_pred = img[i, :, :]
+        classes = np.unique(sagital_pred)
+        if classes.size >= 2:
+            counts = np.array([max(np.where(sagital_pred == c)[0].size, 1e-8) for c in range(num_classes)])
+            score = np.exp(np.sum(np.log(counts)) - num_classes * np.log(np.sum(counts)))
+            if score > max_score:
+                max_score = score
+                max_score_idx = i
+    return max_score_idx
+
+
+def decode_segmap(temp, num_classes):
+    colors = [[0, 0, 0],  # "unlabelled"
+              [150, 0, 0],
+              [0, 0, 142],
+              [150, 170, 0],
+              [70, 0, 100]]
+    label_colours = dict(zip(range(num_classes), colors))  # key: label's index, value: color
+
+    r = temp.copy()
+    g = temp.copy()
+    b = temp.copy()
+    for l in range(1, num_classes):
+        r[temp == l] = label_colours[l][0]
+        g[temp == l] = label_colours[l][1]
+        b[temp == l] = label_colours[l][2]
+
+    rgb = np.zeros((temp.shape[0], temp.shape[1], 3))
+    rgb[:, :, 0] = r / 255.0
+    rgb[:, :, 1] = g / 255.0
+    rgb[:, :, 2] = b / 255.0
+    return rgb
+
+
+def get_train_transform():
+    tr_transforms = []
+
+    tr_transforms.append(GaussianNoiseTransform(p_per_sample=0.1, data_key="image"))
+    tr_transforms.append(
+        GaussianBlurTransform(blur_sigma=(0.5, 1.), different_sigma_per_channel=True,
+                              p_per_channel=0.5, p_per_sample=0.2, data_key="image"))
+    tr_transforms.append(BrightnessMultiplicativeTransform((0.75, 1.25), p_per_sample=0.15, data_key="image"))
+    tr_transforms.append(BrightnessTransform(0.0, 0.1, True, p_per_sample=0.15,
+                                             p_per_channel=0.5, data_key="image"))
+    tr_transforms.append(ContrastAugmentationTransform(p_per_sample=0.15, data_key="image"))
+    tr_transforms.append(
+        SimulateLowResolutionTransform(zoom_range=(0.5, 1), per_channel=True, p_per_channel=0.5,
+                                       order_downsample=0, order_upsample=3, p_per_sample=0.25,
+                                       ignore_axes=None, data_key="image"))
+    tr_transforms.append(GammaTransform(gamma_range=(0.7, 1.5), invert_image=False, per_channel=True,
+                                        retain_stats=True, p_per_sample=0.15, data_key="image"))
+
+    # compose these transforms together
+    tr_transforms = Compose(tr_transforms)
+    return tr_transforms
+
+
+def my_collate(batch):
+    image, label, name = zip(*batch)
+    image = np.stack(image, 0)
+    label = np.stack(label, 0)
+    name = np.stack(name, 0)
+    data_dict = {'image': image,
+                 'label': label,
+                 'name': name}
+    tr_transforms = get_train_transform()
+    data_dict = tr_transforms(**data_dict)
+    return data_dict
