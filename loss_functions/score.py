89957c8616e1a7e3d43d37b7cee8692985ea571f
Talia-Hyeon <030310jhkim@naver.com>
2024-01-20 18:40:39 +0900

    add extend_channel_classes, CrossEntropyFnc, CELoss

--- loss_functions/score.py
+++ loss_functions/score.py
@@ -1,9 +1,22 @@
-import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from data.flare21 import index_organs
+
+def extend_channel_classes(label, num_classes):
+    label_list = []
+    bg = torch.ones_like(label)
+
+    for i in range(1, num_classes):
+        label_i = torch.clone(label)
+        label_i[label == i] = 1
+        label_i[label != i] = 0
+        bg -= label_i
+        label_list.append(label_i)
+
+    label_list = [bg] + label_list
+    concated_label = torch.cat(label_list, dim=1)
+    return concated_label
 
 
 class averageMeter(object):
@@ -55,28 +68,13 @@ class ArgmaxDiceScore(nn.Module):
         # one channel & multi-class
         predict = torch.argmax(predict, dim=1, keepdim=True)  # (B, 1, H, W, D)
         # mutil-channel & binary class
-        predict = self.extend_channel_classes(predict)  # (B, num_classes, H, W, D)
+        predict = extend_channel_classes(predict, num_classes=self.num_classes)  # (B, num_classes, H, W, D)
         for i in range(1, self.num_classes):  # 1: evaluate score from organs(liver)
             dice_score = self.dice(predict[:, i], target[:, i]).unsqueeze(1)  # (B, 1)
             all_score.append(dice_score)  # append each organ
 
         total_score = torch.cat(all_score, dim=1)  # (B, num_classes-1)
         return total_score
-
-    def extend_channel_classes(self, label):
-        label_list = []
-        bg = torch.ones_like(label)
-
-        for i in range(1, self.num_classes):
-            label_i = torch.clone(label)
-            label_i[label == i] = 1
-            label_i[label != i] = 0
-            bg -= label_i
-            label_list.append(label_i)
-
-        label_list = [bg] + label_list
-        concated_label = torch.cat(label_list, dim=1)
-        return concated_label
 
 
 class BinaryDiceLoss(nn.Module):
@@ -183,45 +181,26 @@ class SupervisedLoss(nn.Module):
         return loss
 
 
+class CossEntropyFnc(nn.Module):
+    def __init__(self, smooth=1e-5):
+        super(CossEntropyFnc, self).__init__()
+        self.smooth = smooth
+
+    def forward(self, prediction, label):
+        # flatten
+        prediction = prediction.contiguous().view(-1)
+        label = label.contiguous().view(-1)
+
+        loss = -torch.sum(label * torch.log(prediction + self.smooth))
+        return loss
+
+
 class CELoss(nn.Module):
-    def __init__(self, weight=None, num_classes=5):
+    def __init__(self):
         super(CELoss, self).__init__()
-        self.weight = weight
-        self.num_classes = num_classes
-        self.criterion = cross_entropy_loss
+        self.criterion = CossEntropyFnc()
 
-    def forward(self, predict, target):
-        assert predict.shape == target.shape, 'predict & target shape do not match'
-        predict = F.softmax(predict, dim=1)
-
-        total_loss = []
-        for i in range(self.num_classes):
-            ce_loss = self.criterion(predict[:, i], target[:, i])
-
-            if self.weight is not None:
-                assert len(self.weight) == self.num_classes, \
-                    'do not match length of weight and # of classes'
-                ce_loss *= self.weight[i]
-
-            total_loss.append(ce_loss)  # append each organ
-
-        # msg = 'CELoss '
-        # for k, v in enumerate(total_loss):
-        #     msg += f'| {index_organs[k]}: {v.item()} '
-        # print(msg, end='\r')
-
-        total_loss = torch.stack(total_loss)
-        avg_loss = torch.mean(total_loss)  # mean of all organs
-        return avg_loss
-
-
-def cross_entropy_loss(prediction, label):
-    # prediction = F.softmax(prediction, dim=1)
-
-    # flatten
-    prediction = prediction.view(-1)
-    label = label.view(-1)
-
-    # loss = -torch.sum(label * torch.log(prediction))
-    loss = -torch.sum(label * torch.log(prediction) + (1 - label) * torch.log(1 - prediction))
-    return loss
+    def forward(self, prediction, label):
+        prediction = F.softmax(prediction, dim=1)
+        loss = self.criterion(prediction, label)
+        return loss
