48e4efd80b957820723ae25d55684ba1ab4d39f9
Talia-Hyeon <030310jhkim@naver.com>
2024-01-22 10:41:08 +0900

    delete task_id, adjust data path

--- data/flare21.py
+++ data/flare21.py
@@ -14,17 +14,12 @@ index_organs = ['background', 'liver', 'kidney', 'spleen', 'pancreas']
 
 
 class FLAREDataSet(data.Dataset):
-    def __init__(self, root='../../MOSInversion/dataset/FLARE_Dataset', split='train', task_id=4):
+    def __init__(self, root='../dataset/FLARE_Dataset', split='train'):
         self.root = root
         self.split = split
-        self.task_id = task_id
 
-        # read image path
-        if self.split == 'train':
-            image_path = osp.join(self.root, self.split, str(self.task_id), 'img')
-        else:
-            image_path = osp.join(self.root, self.split, 'img')
-
+        # read data path
+        image_path = osp.join(self.root, self.split, 'img')
         img_list = os.listdir(image_path)
         self.files = load_data(img_list, image_path)
         print("{}'s {} images are loaded!".format(self.split, len(self.files)))
@@ -55,7 +50,7 @@ class FLAREDataSet(data.Dataset):
     def extend_channel_classes(self, label):
         label_list = []
         bg = np.ones_like(label)
-        for i in range(1, self.task_id + 1):  # 1: from foreground
+        for i in range(1, 5):  # 1: from foreground
             label_i = label.copy()
             label_i[label == i] = 1
             label_i[label != i] = 0
@@ -96,12 +91,11 @@ def load_data(data_l, image_path):
 
 if __name__ == '__main__':
     for task_id in range(1, 5):
-        flare = FLAREDataSet(split='train', task_id=task_id)
+        flare = FLAREDataSet(split='train')
 
-    # task_id = 4
     # save_path = '../fig/preprocessed_data/flare'
     # os.makedirs(save_path, exist_ok=True)
-    # flare = FLAREDataSet(root='../dataset/FLARE_Dataset_10', split='val', task_id=task_id)
+    # flare = FLAREDataSet(root='../dataset/FLARE_Dataset', split='val')
     # val_loader = data.DataLoader(dataset=flare, batch_size=1, shuffle=False)
     # for val_iter, pack in enumerate(val_loader):
     #     img_ = pack[0]
