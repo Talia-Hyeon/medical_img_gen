74cf498a5eff1ab887e5d5ff0a677a0022e44e0d
jihyeon <030310jhkim@naver.com>
2024-01-26 15:48:31 +0900

    set seed

--- train_unet.py
+++ train_unet.py
@@ -49,9 +49,10 @@ def main():
     random.seed(seed)
     np.random.seed(seed)
     torch.manual_seed(seed)
-    if torch.cuda.is_available():
-        torch.cuda.manual_seed(seed)
-        torch.backends.cudnn.deterministic = True
+    if device == 'cuda':
+        torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
 
     # make directory
     os.makedirs('./save_model', exist_ok=True)
--- upper_bound.py
+++ upper_bound.py
@@ -33,9 +33,10 @@ def train_upperbound(args):
     random.seed(seed)
     np.random.seed(seed)
     torch.manual_seed(seed)
-    if torch.cuda.is_available():
-        torch.cuda.manual_seed(seed)
-        torch.backends.cudnn.deterministic = True
+    if device == 'cuda':
+        torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
 
     # make directory
     os.makedirs(f'./save_model/{train_type}', exist_ok=True)
