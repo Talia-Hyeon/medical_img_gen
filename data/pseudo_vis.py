e59bc6a29a04fa0b8e958b0b49712af153a8fab9
Talia-Hyeon <030310jhkim@naver.com>
2024-04-20 17:19:15 +0900

    add def visualization

--- data/pseudo_vis.py
+++ data/pseudo_vis.py
@@ -7,25 +7,26 @@ import matplotlib.pyplot as plt
 from torch.utils import data
 
 sys.path.append('..')
-from util import find_best_view
+from util import find_best_view, decode_segmap
 
 
 class PseudoVis(data.Dataset):
-    def __init__(self, npy_root, png_root):
+    def __init__(self, npy_root, png_root, num_classes):
         self.npy_root = npy_root
         self.png_root = png_root
+        self.num_classes = num_classes
 
-        self.npy_list = self.make_npy_list(rootdir=self.npy_root, suffix='npy')
+        self.npy_list = self.make_npy_list()
 
     def __len__(self):
         return len(self.files)
 
-    def make_npy_list(self, rootdir=".", suffix=""):
+    def make_npy_list(self):
         npy_list = list()
 
-        for path, dirs, filenames in os.walk(rootdir):
+        for path, dirs, filenames in os.walk(self.npy_root):
             for filename in filenames:
-                if filename.endswith(suffix):
+                if filename.endswith('npy'):
                     file = osp.join(path, filename)
                     if file.split(os.sep)[-2] == 'img':
                         npy_list.append(file)
@@ -51,6 +52,22 @@ class PseudoVis(data.Dataset):
         self.visualization(image, label, img_iter, img_dir)
 
     def visualization(self, npy_img, npy_label, img_iter, img_dir):
+        # slice into the best view
+        max_score_idx = find_best_view(npy_label, self.num_classes)
+        img = npy_img[max_score_idx, :, :]
+        label = npy_label[max_score_idx, :, :]
+
+        col_label = decode_segmap(img, label, self.num_classes)
+
+        plt.figure()
+        plt.subplot(1, 2, 1)
+        plt.imshow(img)
+        plt.title('Image')
+        plt.subplot(1, 2, 2)
+        plt.imshow(col_label)
+        plt.title('Pseudo GT')
+        plt.savefig(f'{img_dir}/{img_iter}.png')
+        plt.close()
 
 
 if __name__ == '__main__':
@@ -60,7 +77,7 @@ if __name__ == '__main__':
     npy_path = f'../sample/{train_type}'
     os.makedirs(npy_path, exist_ok=True)
 
-    pseudo_images = PseudoVis(npy_root=npy_path, png_root=save_path)
+    pseudo_images = PseudoVis(npy_root=npy_path, png_root=save_path, num_classes=5)
 
     for npy_path in pseudo_images.npy_list:
         pseudo_images.save_png(npy_path)
