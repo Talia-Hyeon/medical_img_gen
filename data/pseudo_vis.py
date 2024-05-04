3c6a8fce0198d9cc8ed2a32ce58e6aef7a29ba22
Talia-Hyeon <030310jhkim@naver.com>
2024-05-04 22:17:46 +0900

    visualization real gt

--- data/pseudo_vis.py
+++ data/pseudo_vis.py
@@ -36,6 +36,9 @@ class PseudoVis(data.Dataset):
 
     def save_png(self, npy_img_path):
         npy_label_path = npy_img_path.replace('img', 'mask')
+        img_dirname = osp.dirname(npy_img_path)
+        target_dirname = img_dirname.replace('img', 'label')
+        npy_target_path = osp.join(target_dirname, 'final.npy')
 
         img_path_split = npy_img_path.split(os.sep)
         img_iter = img_path_split[-1]
@@ -46,59 +49,41 @@ class PseudoVis(data.Dataset):
         # load numpy array
         image = np.load(npy_img_path)
         label = np.load(npy_label_path)
+        target = np.load(npy_target_path)
 
         image = image.astype(np.float32)
         label = label.astype(np.float32)
+        target = target.astype(np.float32)
 
-        if npy_label_path.find('final') != -1:
-            npy_target_path = npy_img_path.replace('img', 'label')
-            target = np.load(npy_target_path)
-            target = target.astype(np.float32)
-            self.visualization(image, label, img_iter, img_dir, target)
+        self.visualization(image, label, target, img_iter, img_dir)
 
-        else:
-            self.visualization(image, label, img_iter, img_dir)
-
-    def visualization(self, npy_img, npy_label, img_iter, img_dir, npy_target=None):
+    def visualization(self, npy_img, npy_label, npy_target, img_iter, img_dir):
         # remove batch
         npy_img = np.squeeze(npy_img)
         npy_label = np.squeeze(npy_label)
+        npy_target = np.squeeze(npy_target)
 
         # slice into the best view
         max_score_idx = find_best_view(npy_label, self.num_classes)
         img = npy_img[max_score_idx, :, :]
         label = npy_label[max_score_idx, :, :]
+        target = npy_target[max_score_idx, :, :]
 
         col_label = decode_segmap(label, self.num_classes)
+        col_target = decode_segmap(target, self.num_classes)
 
-        if npy_target != None:
-            npy_target = np.squeeze(npy_target)
-            target = npy_target[max_score_idx, :, :]
-            col_target = decode_segmap(target, self.num_classes)
-
-            plt.figure()
-            plt.subplot(1, 3, 1)
-            plt.imshow(img, cmap='gray')
-            plt.title('Image')
-            plt.subplot(1, 3, 2)
-            plt.imshow(col_label)
-            plt.title('Pseudo GT')
-            plt.subplot(1, 3, 3)
-            plt.imshow(col_target)
-            plt.title('Real GT')
-            plt.savefig(f'{img_dir}/{img_iter}.png')
-            plt.close()
-
-        else:
-            plt.figure()
-            plt.subplot(1, 2, 1)
-            plt.imshow(img, cmap='gray')
-            plt.title('Image')
-            plt.subplot(1, 2, 2)
-            plt.imshow(col_label)
-            plt.title('Pseudo GT')
-            plt.savefig(f'{img_dir}/{img_iter}.png')
-            plt.close()
+        plt.figure()
+        plt.subplot(1, 3, 1)
+        plt.imshow(img, cmap='gray')
+        plt.title('Image')
+        plt.subplot(1, 3, 2)
+        plt.imshow(col_label)
+        plt.title('Pseudo GT')
+        plt.subplot(1, 3, 3)
+        plt.imshow(col_target)
+        plt.title('Real GT')
+        plt.savefig(f'{img_dir}/{img_iter}.png')
+        plt.close()
 
 
 if __name__ == '__main__':
