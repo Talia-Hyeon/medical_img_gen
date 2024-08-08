7500af1002851faac83095c96dda718bd8763e21
jihyeon <030310jhkim@naver.com>
2024-08-08 21:33:33 +0900

    add log img & add vis_img_log.py

--- util/vis_img_log.py
+++ util/vis_img_log.py
@@ -8,8 +8,8 @@ import matplotlib.pyplot as plt
 sys.path.append('..')
 
 
-def make_tot_log_npy(log_root):
-    os.makedirs(f'{log_root}/mean', exist_ok=True)
+def make_tot_log_npy(root_dir, weight):
+    log_root = osp.join(root_dir, 'di_mask dice ' + weight)
     tot_l1 = list()
     tot_l2 = list()
     tot_bn = list()
@@ -37,73 +37,69 @@ def make_tot_log_npy(log_root):
 
     l1_log = np.array(tot_l1)
     l1_mean = l1_log.mean(axis=0)
-    np.save(f'{log_root}/mean/l1.npy', l1_mean)
+    np.save(f'{root_dir}/l1/{weight}.npy', l1_mean)
 
     l2_log = np.array(tot_l2)
     l2_mean = l2_log.mean(axis=0)
-    np.save(f'{log_root}/mean/l2.npy', l2_mean)
+    np.save(f'{root_dir}/l2/{weight}.npy', l2_mean)
 
     bn_log = np.array(tot_bn)
     bn_mean = bn_log.mean(axis=0)
-    np.save(f'{log_root}/mean/bn.npy', bn_mean)
+    np.save(f'{root_dir}/bn/{weight}.npy', bn_mean)
 
     dice_log = np.array(tot_dice)
     dice_mean = dice_log.mean(axis=0)
-    np.save(f'{log_root}/mean/dice.npy', dice_mean)
+    if weight == '1e-2':
+        dice_mean = dice_mean * 100.0
+    np.save(f'{root_dir}/dice/{weight}.npy', dice_mean)
 
 
-def make_plot(root):
-    l1_log = np.load(f'{root}/l1.npy')
-    l2_log = np.load(f'{root}/l2.npy')
-    bn_log = np.load(f'{root}/bn.npy')
-    dice_log = np.load(f'{root}/dice.npy')
+def save_tot_loss(root, weight):
+    l1_log = np.load(f'{root}/l1/{weight}.npy')
+    l2_log = np.load(f'{root}/l2/{weight}.npy')
+    bn_log = np.load(f'{root}/bn/{weight}.npy')
+    dice_log = np.load(f'{root}/dice/{weight}.npy')
+    tot_log = l1_log + l2_log + bn_log + dice_log * 10.0
+    np.save(f'{root}/tot/{weight}.npy', tot_log)
 
-    plt.plot(l1_log, 'y', label='L1')
-    plt.plot(l2_log, 'g', label='L2')
-    plt.plot(bn_log, 'b', label='Batch_Norm')
-    plt.plot(dice_log, 'r', label='Dice')
+
+def loss_plot(root, loss_type):
+    loss_path = osp.join(root, loss_type)
+    weight_list = os.listdir(loss_path)
+    for i in weight_list:
+        path = osp.join(loss_path, i)
+        if i.find('1e-2') != -1:
+            weight_em2 = np.load(path)
+        elif i.find('10') != -1:
+            weight_10 = np.load(path)
+        else:
+            weight_1 = np.load(path)
+
+    plt.plot(weight_em2, 'g', label='1e-2')
+    plt.plot(weight_1, 'b', label='1.0')
+    plt.plot(weight_10, 'r', label='10.0')
     plt.legend(loc='upper right')
     plt.yscale('log')
-    plt.ylim(0, 10)
-    plt.savefig(f'{root}/Log of generating images.png')
-
-
-def save_tot_loss(root):
-    l1_log = np.load(f'{root}/l1.npy')
-    l2_log = np.load(f'{root}/l2.npy')
-    bn_log = np.load(f'{root}/bn.npy')
-    dice_log = np.load(f'{root}/dice.npy')
-    tot_log = l1_log + l2_log + bn_log + dice_log * 10.0
-    np.save(f'{root}/tot.npy', tot_log)
-
-
-def dice_plot(root):
-    dice_list = os.listdir(root)
-    for i in range(len(dice_list)):
-        path = osp.join(root, dice_list[i])
-
-        if dice_list[i].find('1e-2') != -1:
-            dice_em2 = np.load(path)
-        elif dice_list[i].find('10') != -1:
-            dice_10 = np.load(path)
-        else:
-            dice_1 = np.load(path)
-
-    plt.plot(dice_em2, 'g', label='Weight of Dice Loss: 1e-2')
-    plt.plot(dice_1, 'b', label='Weight of Dice Loss: 1.0')
-    plt.plot(dice_10, 'r', label='Weight of Dice Loss: 10.0')
-    plt.legend(loc='upper right')
-    # plt.yscale('log')
-    plt.ylim(0, 0.6)
+    # plt.ylim(0, 12)
     plt.grid(True)
-    plt.savefig(f'{root}/Log of bn.png')
+    plt.savefig(f'{root}/{loss_type} Loss.png')
+    plt.close()
 
 
 if __name__ == '__main__':
-    # log_root = '../log_img/di_mask dice 1'
-    # make_tot_log_npy(log_root)
-    # make_plot(f'{log_root}/mean')
+    root_list = ['1e-2', '1', '10']
+    root_dir = '../log_img'
 
-    # save_tot_loss('../log_img/di_mask dice 1e-2')
-    root = '../log_img/bn'
-    dice_plot(root)
+    os.makedirs(f'{root_dir}/l1', exist_ok=True)
+    os.makedirs(f'{root_dir}/l2', exist_ok=True)
+    os.makedirs(f'{root_dir}/bn', exist_ok=True)
+    os.makedirs(f'{root_dir}/dice', exist_ok=True)
+    os.makedirs(f'{root_dir}/tot', exist_ok=True)
+
+    for weight in root_list:
+        make_tot_log_npy(root_dir, weight)
+        save_tot_loss(root_dir, weight)
+
+    loss_list = ['l1', 'l2', 'bn', 'dice', 'tot']
+    for loss_type in loss_list:
+        loss_plot(root_dir, loss_type)
